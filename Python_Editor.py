f14f7b014ea908d8379360a241c67162cd6bb235
Ayman Abd El-Hadi <hunter.me33@gmail.com>
2018-06-07 22:30:18 +0200

    Update Python_Editor.py

--- Python_Editor.py
+++ Python_Editor.py
@@ -4,8 +4,9 @@ from PyQt5.QtWidgets import QMainWindow , QFileDialog , QApplication , qApp ,QLi
 from PyQt5.uic import loadUiType
 from os import path
 from sys import argv
+import re
 
-#>----------------------------------------------------------------------------------------------------------------<
+#>---------------------------------------------------------------------------------------------------------------------<
 
 FORM_CLASS,_= loadUiType(path.join(path.dirname(__file__),"main.ui"))
 
@@ -16,7 +17,8 @@ class MainApp(QMainWindow, FORM_CLASS):
         self.setupUi(self)
         self.Handle_Buttons()
         self.Recognizer()
-        self.file_name = None
+        self.Auther = "//Author : Cyber_hunter\n\n"
+        self.plainTextEdit.insertPlainText(self.Auther)
 
     def Handle_Buttons(self):
         self.actionNew_File.triggered.connect(self.New_file)
@@ -31,36 +33,45 @@ class MainApp(QMainWindow, FORM_CLASS):
         self.actionZoom_in.setShortcut("Ctrl++")
         self.actionZoom_out.triggered.connect(self.Zoom_Out)
         self.actionZoom_out.setShortcut("Ctrl+-")
-        self.actionUndo.triggered.connect(self.Undo)
-        self.actionUndo.setShortcut("Ctrl+Shift+U")
         self.actionRedo.triggered.connect(self.Redo)
-        self.actionRedo.setShortcut("Ctrl+Shift+R")
+        self.actionRedo.setShortcut("Ctrl+R")
 
     def New_file(self):
         global file_name
         file_name = "Untitled"
         self.plainTextEdit.clear()
+        self.plainTextEdit.insertPlainText(self.Auther)
 
     def Open_file(self):
-        self.file_name = QFileDialog.getOpenFileName(self ,directory='.' , filter= "*.txt *.py")
-        self.plainTextEdit.clear()
-        if self.file_name:
-            with open(self.file_name[0], "rt") as file:
-                self.plainTextEdit.insertPlainText(file.read())
+        try:
+            self.file_name = QFileDialog.getOpenFileName(self ,directory='.' , filter= "*.py *.java")
+            print(self.file_name)
+            x = open(self.file_name[0], "rt")
+            self.plainTextEdit.clear()
+            self.plainTextEdit.insertPlainText(self.Auther)
+            self.plainTextEdit.insertPlainText(x.read())
+        except Exception as e:
+            print(e)
 
     def Save_file(self):
         text = self.plainTextEdit.toPlainText()
-        self.file_name = QFileDialog.getSaveFileName(self, directory='.' , filter="*.py")
-        with open(self.file_name[0], "wt") as file:
-            file.write(text)
-            file.close()
+        try:
+            self.file_name = QFileDialog.getSaveFileName(self, directory='.' , filter="*.py *.java")
+            with open(self.file_name[0], "wt") as file:
+                file.write(text)
+                file.close()
+        except Exception as e :
+            print(e)
 
     def SaveAs_file(self):
         text = self.plainTextEdit.toPlainText()
-        self.file_name = QFileDialog.getSaveFileName(self, directory='.' ,filter="*.py")
-        with open(self.file_name[0], "wt") as file:
-            file.write(text)
-            file.close()
+        try:
+            self.file_name = QFileDialog.getSaveFileName(self, directory='.' ,filter="*.py *.java")
+            with open(self.file_name[0], "wt") as file:
+                file.write(text)
+                file.close()
+        except Exception as e:
+            print(e)
 
     def Zoom_In(self):
         self.plainTextEdit.zoomIn(range=2)
@@ -78,34 +89,145 @@ class MainApp(QMainWindow, FORM_CLASS):
         self.editor = self.plainTextEdit.document()
         self.highlighter = Recognition(self.editor)
 
+
 #>---------------------------------------------------------------------------------------------------------------------<
+
 
 def format(color , style = ''):
-    _color = QColor()
-    _color.setNamedColor(color)
-    _format = QTextCharFormat()
-    _format.setForeground(_color)
+    set_color = QColor()
+    set_color.setNamedColor(color)
+    format = QTextCharFormat()
+    format.setForeground(set_color)
     if "bold" in style :
-        _format.setFontWeight(QFont.Bold)
-    elif "italic" in style :
-        _format.setFontItalic(True)
-    elif "underline" in style :
-        _format.setFontUnderline(True)
+        format.setFontWeight(QFont.Bold)
+    if "italic" in style :
+        format.setFontItalic(True)
+    if "underline" in style :
+        set_color.setNamedColor("red")
+        format.setFontUnderline(True)
+        format.setUnderlineColor(set_color)
 
-    return _format
+    return format
 
 STYLES = {
-    'keyword': format('cyan' ,'bold'),
-    'operator': format('red'),
-    'brace': format('brown' , 'bold'),
-    'comment': format('gray', 'italic'),
-    'string': format('magenta'),
-    'numbers': format('yellow'),
+    'keyword'  : format('cyan' ,'bold') ,
+    'operator' : format('red') ,
+    'brace'    : format('brown') ,
+    'comment'  : format('gray', 'italic , bold') ,
+    'string'   : format('magenta') ,
+    'numbers'  : format('yellow') ,
     'class'    : format('blue' , 'bold') ,
+    'default'  : format('white') ,
     'error'    : format('white' , 'underline') ,
-}
+    'operation': format('green' , 'bold')
+    }
 
 #>---------------------------------------------------------------------------------------------------------------------<
+
+INTEGER, PLUS, MINUS, MULT, DIV, EOF = 'INTEGER', 'PLUS', 'MINUS', 'MULT' , 'DIV', 'EOF'
+
+class Token(object):
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+
+class Interpreter(object):
+    def __init__(self, text):
+        self.text = text
+        self.pos = 0
+        self.current_token = None
+        self.current_char = self.text[self.pos]
+
+    def error(self):
+        raise Exception('Error parsing input')
+
+    def advance(self):
+        self.pos += 1
+        if self.pos > len(self.text) - 1:
+            self.current_char = None
+        else:
+            self.current_char = self.text[self.pos]
+
+    def skip_whitespace(self):
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance()
+
+    def integer(self):
+        result = ''
+        while self.current_char is not None and self.current_char.isdigit():
+            result += self.current_char
+            self.advance()
+        return int(result)
+
+    def get_next_token(self):
+        while self.current_char is not None:
+
+            if self.current_char.isspace():
+                self.skip_whitespace()
+                continue
+
+            if self.current_char.isdigit():
+                return Token(INTEGER, self.integer())
+
+            if self.current_char == '+':
+                self.advance()
+                return Token(PLUS, '+')
+
+            if self.current_char == '-':
+                self.advance()
+                return Token(MINUS, '-')
+
+            if self.current_char == '*':
+                self.advance()
+                return Token(MULT, '*')
+
+            if self.current_char == '/':
+                self.advance()
+                return Token(DIV, '/')
+
+            self.error()
+
+        return Token(EOF, None)
+
+    def eat(self, token_type):
+        if self.current_token.type == token_type:
+            self.current_token = self.get_next_token()
+        else:
+            self.error()
+
+    def expr(self):
+        self.current_token = self.get_next_token()
+
+        left = self.current_token
+        self.eat(INTEGER)
+
+        op = self.current_token
+        op = self.current_token
+        if op.type == PLUS:
+            self.eat(PLUS)
+        elif op.type == MINUS:
+            self.eat(MINUS)
+        elif op.type == MULT:
+            self.eat(MULT)
+        elif op.type == DIV:
+            self.eat(DIV)
+
+        right = self.current_token
+        self.eat(INTEGER)
+
+        if op.type == PLUS:
+            result = left.value + right.value
+        elif op.type == MINUS:
+            result = left.value - right.value
+        elif op.type == MULT:
+            result = left.value * right.value
+        elif op.type == DIV:
+            result = left.value / right.value
+        return result
+
+
+#>---------------------------------------------------------------------------------------------------------------------<
+
 
 class Recognition(QSyntaxHighlighter):
     keywords = [
@@ -131,22 +253,75 @@ class Recognition(QSyntaxHighlighter):
 
         rules = []
 
-        rules += [(r'\b%s\b' % w, 0, STYLES['keyword'])
-                  for w in Recognition.keywords]
-        rules += [(r'%s' % o, 0, STYLES['operator'])
-                  for o in Recognition.operators]
-        rules += [(r'%s' % b, 0, STYLES['brace'])
-                  for b in Recognition.braces]
-        rules += [(r'//[^\n]*', 0, STYLES['comment']) ,
-                  (r'"[^"\\]*(\\.[^"\\]*)*.', 0, STYLES['error']),
-                  (r'"[^"\\]*(\\.[^"\\]*)*"', 0, STYLES['string']),
-                  (r"'[^'\\]*(\\.[^'\\]*)*.", 0, STYLES['error']),
-                  (r'\'[^\'\\]*(\\.[^\'\\]*)*\'', 0, STYLES['string']),
-                  (r'\b?[0-9]+[lL]?\b', 0, STYLES['numbers']),
-                  (r'\bclass\b\s*(\w+)', 1, STYLES['class'])]
+        rules += [
+                  ('[^\s.*.]', 0, STYLES['error']),
+                  (r'[:]', 0, STYLES['default'])]
 
-        self.rules = [(QRegExp(pat), index, fmt)
-                      for (pat, index, fmt) in rules]
+        rules += [
+                  (r'\b%s\b\s*(\w+(\.)\w+)' % x, 1, STYLES['default'])                      #
+                        for x in Recognition.keywords]
+
+        rules += [
+            (r'\b%s\b\s*(\w+\s*(\,)*\s*\w+)' % x, 1, STYLES['default'])  #
+            for x in Recognition.keywords]
+
+        rules += [
+                  (r'\b%s\b\s*(\w+)' % x, 1, STYLES['default'])  #
+                        for x in Recognition.keywords]
+
+        rules += [
+                  (r'\([^\)\\]*(\\.[^\(\\]*)*.', 0, STYLES['error']),
+                  (r'\([^\)\\]*(\\.[^\(\\]*)*\)', 0, STYLES['default']),
+                  (r'\[[^\]\\]*(\\.[^\[\\]*)*.', 0, STYLES['error']),
+                  (r'\[[^\]\\]*(\\.[^\[\\]*)*\]', 0, STYLES['default']),
+                  (r'\{[^\}\\]*(\\.[^\{\\]*)*.', 0, STYLES['error']),
+                  (r'\{[^\}\\]*(\\.[^\{\\]*)*\}', 0, STYLES['default'])]
+
+        rules += [(r'(.*.)=', 0, STYLES['default'])]
+
+        rules += [
+                 (r'\b%s\b' % w, 0, STYLES['keyword'])
+                        for w in Recognition.keywords]
+
+        rules += [
+                  (r'%s' % o, 0, STYLES['operator'])
+                        for o in Recognition.operators]
+
+        rules += [
+                  (r'\b[0-9]+[eE]?\b', 0, STYLES['numbers']) ,
+                  (r'"[^"\\]*(\\.[^"\\]*)*.', 0, STYLES['error']) ,
+                  (r'"[^"\\]*(\\.[^"\\]*)*"', 0, STYLES['string']) ,
+                  (r"'[^'\\]*(\\.[^'\\]*)*.", 0, STYLES['error']) ,
+                  (r'\'[^\'\\]*(\\.[^\'\\]*)*\'', 0, STYLES['string'])]
+
+        rules += [
+                  (r'\bclass\b\s*(\w+)', 1, STYLES['error']) ,
+                  #(r'\bclass\b\s*(\w+\s*(\(\))\s*(\:))', 1, STYLES['class']) ,
+                  (r'\bclass\b\s*([0-9]*\w*)', 1, STYLES['error']),
+                  (r'\b\bclass\b\s*(\w+)\b\s*(\((\w*)\))\s*(\:)', 1, STYLES['class']),
+                  (r'\b\bclass\b\s*(\w+)\b\s*(\((\w*)\))\s*(\:)', 4, STYLES['class']),
+                  (r'\bdef\b\s*(\w+)', 1, STYLES['error']),
+                  (r'\bdef\b\s*(\w+\s*(\(\))\s*(\:))', 1, STYLES['class']),
+                  (r'\bdef\b\s*([0-9]\w+)', 1, STYLES['error']),
+                  (r'\b\bdef\b\s*(\w+)\b\s*(\((\w+)\))\s*(\:)', 1, STYLES['class']),
+                  (r'\b\bdef\b\s*(\w+)\b\s*(\((\w+)\))\s*(\:)', 4, STYLES['class']),]
+
+        rules += [
+                 (r'%s' % b, 0, STYLES['brace'])
+                        for b in Recognition.braces]
+
+        rules += [(r'#[^\n]*', 0, STYLES['comment'])]
+
+        rules += [(r'\bfor\b\s*(\w+)\s(\w+)\s(\w+)(\([0-9]+\))\:',0,STYLES['operation']),
+                  (r'[0-9]+\s*\+\s*[0-9]+;', 0, STYLES['operation']),
+                  (r'[0-9]+\s*\-\s*[0-9]+;', 0, STYLES['operation']),
+                  (r'[0-9]+\s*\*\s*[0-9]+;', 0, STYLES['operation']),
+                  (r'[0-9]+\s*\/\s*[0-9]+;', 0, STYLES['operation'])]
+
+        self.rules = [
+                      (QRegExp(pat), index, fmt)
+                            for (pat, index, fmt) in rules
+                     ]
 
     def highlightBlock(self, text):
         for expression, nth, format in self.rules:
@@ -156,6 +331,16 @@ class Recognition(QSyntaxHighlighter):
                 length = len(expression.cap(nth))
                 self.setFormat(index, length, format)
                 index = expression.indexIn(text, index + length)
+
+        operations = ['\+', '\-','\*','\/']
+        op_rules = []
+        op_rules += [re.findall('[0-9]+\s*{}\s*[0-9]+;'.format(x), text)
+                     for x in operations]
+        for rule in op_rules:
+            if rule != []:
+                parser = Interpreter(rule[0].replace(';',''))
+                result = parser.expr()
+                print(result)
 
         self.setCurrentBlockState(0)
 
